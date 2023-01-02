26f882293b82a1b914e1872169bcdeaa2036c315
sou7 <soukouki0@yahoo.co.jp>
2023-01-03 00:15:24 +0900

    せっかく引数入れ替えたのに置き換えるの忘れてた

--- i3/check-and-start.sh
+++ i3/check-and-start.sh
@@ -5,10 +5,10 @@ process_search_term=$1
 exec_command=$2
 workspace=$3
 
-i3-msg workspace number $3
+i3-msg workspace number $workspace
 
-if ! pgrep -x $1; then
-    i3-msg mode "check for starting $2 [retype: exec $2]"
+if ! pgrep -x $process_search_term; then
+    i3-msg mode "check for starting $exec_command [retype: exec $exec_command]"
     sleep 0.5
     i3-msg mode default
 fi
